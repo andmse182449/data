c9fb4780ce6b242740f05106473529762129e3ed
Dương Minh An <145546848+andmse182449@users.noreply.github.com>
2023-11-02 13:56:31 +0700

    Update Trigger(Completed).sql

--- Trigger(Completed).sql
+++ Trigger(Completed).sql
@@ -1,126 +1,125 @@
-/* BẢNG TRIGGER 
-   Trigger 1 : Trigger ngăn câu lệnh xóa sách
-   Trigger 2 : Trigger cập nhật giá tiền, số lượng tồn kho của sách sau khi xuất hóa đơn bán hàng 
-   Trigger 3 : Trigger cập nhật trạng thái đơn hàng
+/*
+   PROC1: Procedure với giá trị đầu vào lần lượt là tên sách và kiểu đánh giá, giá trị đầu ra là số lượt đánh giá của kiểu đánh giá đã cho
+   PROC2: Procedure với giá trị đầu vào là tên tác giả, lấy ra tên, giá bán và nhà xuất bản của những quyển sách thuộc về tác giả đã cho
+   PROC3: Procedure với giá trị đầu vào là tên danh mục, lấy ra tên và giá bán của những quyển sách trong danh mục đã cho
+   PROC4: Procedure với giá trị đầu vào lần lượt là tháng và năm bất kì, lấy ra thông tin về những quyển sách bán chạy nhất trong tháng và năm tương ứng 
+   (tiêu chí bán chạy được tính bằng số sách được bán lớn hơn 2)
+   PROC5: Procedure với giá trị đầu vào là một chuỗi kí tự, lấy ra thông tin gồm tên đầy đủ của sách,
+   tên tác giả, giá tiền và nhà xuất bản có chứa chuỗi kí tự tương tự
+   PROC6: Procedure với giá trị đầu vào là mã đơn hàng, procedure sẽ tự cập nhật trạng thái đơn hàng, 
+   đồng thời kết hợp với trigger 3 để ngăn chặn việc cập thái được cập nhật một cách bất thường
 */
---TR1
-create TRIGGER TrgDeleteBook
-ON sach
-instead of DELETE
-AS 
-BEGIN
-    IF EXISTS (SELECT MASACH FROM sach WHERE MASACH = (SELECT MASACH FROM deleted))
-    BEGIN
-        PRINT N'Không thể thực thi lệnh này!'
-        ROLLBACK TRAN
-    END
-END;
-go
--- thực thi
-delete from SACH WHERE MASACH = '3D69F211-918D-4C71-A873-DF4320A8F681'
-insert into SACH(TENSACH, GIATIEN, SLTONKHO, NHAXUATBAN, MAPHATHANH) values(N'oke con de', 23000, 43, 'tgq', 11)
-select * from SACH where TENSACH = N'oke con de'
---TR2			
-ALTER TRIGGER TrgaddOrder
-ON [dbo].[DONHANG_CHITIET] 
-for INSERT, update
-AS 
-BEGIN
-declare @buy int 
-set @buy = (select soluong from inserted)
-if	(SELECT s.SLTONKHO FROM sach s where s.MASACH in (select MASACH from inserted)) >= @buy
-begin
-   update SACH 
-	set  SLTONKHO = SLTONKHO - (select sum(soluong)
-									from inserted b
-									where SACH.MASACH = b.MASACH)
-	where SACH.MASACH in (select MASACH from inserted)
-	
-   UPDATE DONHANG
-	SET tongtien_donhang = tongtien_donhang + (
-		SELECT SUM(s.GIATIEN * a.soluong) - (SUM(s.GIATIEN * a.soluong) * t.ptgiam)						 
-	   FROM inserted a
-		JOIN SACH s ON a.MASACH = s.MASACH
-		JOIN DONHANG b ON a.MADH = b.MADH
-		JOIN KHACHHANG k ON k.MAKH = b.MAKH
-		JOIN tichluy t ON k.matichluy = t.MATL
-		WHERE b.MADH IN (SELECT MADH FROM inserted)
-    GROUP BY t.ptgiam
+-- PROC1
+create procedure proc_1(@tensach nvarchar(max), @kieudanhgia int,@soluongdanhgia int output)
+as
+	begin
+	set @soluongdanhgia = (
+	select COUNT(DANHGIA) 
+	from sach s join BINHLUAN b on s.MASACH = b.MASACH and s.TENSACH = @tensach
+	where DANHGIA = @kieudanhgia
 	)
-WHERE MADH IN (SELECT MADH FROM inserted) 
-
---THUC THI
-	update DONHANG set trangthai = 1 where MADH in (select MADH from inserted)
+		select b.DANHGIA, @soluongdanhgia  N'Số lượt đánh giá'
+		from sach s join BINHLUAN b on s.MASACH = b.MASACH and s.TENSACH = @tensach and b.DANHGIA = @kieudanhgia
+		group by b.DANHGIA
 	end
-	else 
-	begin 
-	print N'Số lượng sản phẩm trong kho không đủ '
-	ROLLBACK TRAN
-	end
-END
 go
---===============================================
-update DONHANG_CHITIET set soluong = 1 where MADH = 1078 and MASACH = '3CCCED2D-F67C-40E2-A52F-04BBC0798BFC'
-select * from SACH where MASACH = '3CCCED2D-F67C-40E2-A52F-04BBC0798BFC' 
-
-select * from DONHANG_CHITIET
-select * from DONHANG
-DELETE FROM DONHANG WHERE MADH = 1078
-DELETE FROM DONHANG_CHITIET WHERE MADH = 1079
-insert into donhang values (N'Nguyễn Tài',351-664-9650,'585 Surrey Avenue',18,5,1,N'trả khi nhận hàng','20230630','20230705',NULL,0,null)
-
-insert into DONHANG_CHITIET values (1079,'3CCCED2D-F67C-40E2-A52F-04BBC0798BFC',1, NULL)
-update DONHANG_CHITIET set soluong = 2 where MADH = 1079 and MASACH = '73241A6A-3836-40F8-AFBD-E2EDEEF54C15'
-update DONHANG set tongtien_donhang = 0 where MADH = 1079 
-insert into DONHANG_CHITIET values	(1079,'73241A6A-3836-40F8-AFBD-E2EDEEF54C15',1, NULL)
---TRIGGER 4
-alter TRIGGER TrgUpdateStatus
-ON DONHANG
-after UPDATE
-AS 
-BEGIN
-if update(TRANGTHAI)
-BEGIN
-	declare @date date
-	select @date = NGAYDUDINHSHIP from DONHANG where MADH in (select MADH from inserted)
+declare @t int 
+execute proc_1 'IELTS Analyst Second Edition', 4, @soluongdanhgia = @t output
+print @t
+go
+-- PROC2
+create procedure proc_2 (@tentg nvarchar(max))
+as
+	begin
+		select c.TENSACH, c.GIATIEN, c.NHAXUATBAN
+		from SANGTAC a, TACGIA b, SACH c
+		where b.MATG = a.MATG and a.MASACH = c.MASACH and b.TENTG = @tentg
+	end
+go
+execute proc_2 @tentg = 'NXB Sheth'
+go
+-- PROC3
+create procedure proc_3(@tendm nvarchar(200))
+as
+	begin
+		select b.TENSACH, b.GIATIEN
+		from [dbo].[DANHMUC] a, SACH b
+		where a.MaDM = b.MADM and a.TenDM = @tendm
+	end
+go
+execute proc_3 @tendm = N'Sách Chuyên Ngành'
+go
+-- PROC4
+create procedure proc_4(@month int, @year int)
+as
+	begin
+		select b.TENSACH, b.GIATIEN, b.NHAXUATBAN
+		from [dbo].[DONHANG] a, SACH b, DONHANG_CHITIET c
+		where a.MADH = c.MADH and b.MASACH = c.MASACH and month(a.NGAYDATHANG) = @month and year(a.NGAYDATHANG) = @year
+		group by b.TENSACH, b.GIATIEN, b.NHAXUATBAN
+		having count(c.MASACH) >= 2
+	end
+go  
+execute proc_4 @month = 9, @year = 2023
+go
+-- PROC5
+create procedure proc_5(@ten nvarchar(max))
+as
+	begin
+		select  s.TENSACH, tg.TENTG ,s.GIATIEN, s.NHAXUATBAN
+		from sach s join SANGTAC st on s.MASACH = st.MASACH join TACGIA tg on tg.MATG = st.MATG
+		where TENSACH like '%' + @ten + '%'
+	end
+go
+execute proc_5 @ten = 'Second'
+-- PROC6
+alter procedure proc_6(@madh int)
+as
+	begin
+declare @date date
+	select @date = NGAYDUDINHSHIP from DONHANG where MADH = @madh
 	declare @e int
 	set @e = DATEDIFF(day,GETDATE(), @date)
 	--=========================
-	if @e <= -7 and (select TRANGTHAI from inserted) = 3 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1
-	begin update DONHANG set trangthai = 3 where MADH = (select MADH from inserted) 
+	if @e <= -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) >= 1
+	begin update DONHANG set trangthai = 3 where MADH = @madh 
 	end
-	ELSE IF @e <= -7  and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1 and (select TRANGTHAI from inserted) = 1 
-	or @e <= -7  and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1 and (select TRANGTHAI from inserted) = 2
-	begin 
-	print N'Đơn hàng đã được giao'
-	rollback tran
+	else if @e <= -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) = 0
+	begin update DONHANG set trangthai = 4 where MADH = @madh 
 	end
 	--=========================
-	else if @e < 0 and  @e > -7 and (select TRANGTHAI from inserted) = 2 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1
+	else if @e < 0 and  @e > -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) >= 1
 	begin
-	update DONHANG set trangthai = 2 where MADH = (select MADH from inserted)
+	update DONHANG set trangthai = 2 where MADH = @madh
 	end
-	ELSE IF @e < 0 and  @e > -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1 and (select TRANGTHAI from inserted) = 1 
-	or @e < 0 and  @e > -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1  and (select TRANGTHAI from inserted) = 3
-	begin 
-	print N'Đơn hàng đang được giao'
-	rollback tran
-	end
-	else if (select TRANGTHAI from inserted) = 4 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) >= 1 
-	begin 
-	print N'Đơn hàng không được phép hủy'
-	rollback tran
-	end
-	else if (select TRANGTHAI from inserted) != 4 and (select count(MADH) from DONHANG_CHITIET dc where MADH = (select MADH from inserted)) = 0 
-	begin 
-	print N'Đơn hàng không được phép giao'
-	rollback tran
+	else if @e < 0 and  @e > -7 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) = 0
+	begin update DONHANG set trangthai = 4 where MADH = @madh 
 	end
 	--=========================
-END
-END
+	else if @e > 0 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) >= 1
+	begin
+	update DONHANG set trangthai = 1 where MADH = @madh
+	end
+	else if @e > 0 and (select count(MADH) from DONHANG_CHITIET dc where MADH = @madh) = 0
+	begin update DONHANG set trangthai = 4 where MADH = @madh 
+	end
+	end
 go
-update DONHANG set TRANGTHAI = 1 where MADH = 31
-update DONHANG SET TRANGTHAI = 4 where MADH = 32
-update DONHANG SET TRANGTHAI = 1 where MADH = 1080
-SELECT *FROM DONHANG
+update DONHANG set TRANGTHAI = 4 where MADH = 34
+execute proc_6 @madh = 33
+execute proc_6 @madh = 34
+execute proc_6 @madh = 35
+select * from DONHANG where MADH in (33,34,35)
+
+
+
+
+
+
+
+
+
+
+
+
+
