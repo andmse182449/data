78b3d30bfcd83e39aa912785590cde4c9b9b85cc
Dương Minh An <145546848+andmse182449@users.noreply.github.com>
2023-11-06 11:29:23 +0700

    Update Trigger(Completed).sql

--- Trigger(Completed).sql
+++ Trigger(Completed).sql
@@ -1,3 +1,9 @@
+/* BẢNG TRIGGER 
+   Trigger 1 : Trigger ngăn câu lệnh xóa sách
+   Trigger 2 : Trigger cập nhật giá tiền, số lượng tồn kho của sách sau khi xuất hóa đơn bán hàng 
+   Trigger 3 : Trigger cập nhật trạng thái đơn hàng
+   Trigger 4 : Trigger kiểm tra ngày tháng đặt hàng có hợp lệ hay không
+*/
 alter TRIGGER TrgUpdateStatus
 ON DONHANG
 after UPDATE
@@ -55,4 +61,22 @@ BEGIN
 	--=========================
 END
 END
+go
+-- TR4 
+CREATE TRIGGER TrgCheckOrderTime
+ON [dbo].[DONHANG] 
+for INSERT
+AS 
+BEGIN
+	IF(SELECT NGAYDATHANG FROM inserted ) > (SELECT NGAYDUDINHSHIP FROM inserted )
+	BEGIN 
+	print 'Thời gian dự định vận chuyển không hợp lệ!'
+	rollback tran
+	END
+	else IF(SELECT NGAYDATHANG FROM inserted ) > getdate()
+	BEGIN 
+	print 'Thời gian đặt hàng không hợp lệ!'
+	rollback tran
+	END
+end
 go
